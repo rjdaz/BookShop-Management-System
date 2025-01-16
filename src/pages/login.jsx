8afb2163094aad239ceb5180d0dff059e6711e85
ban <jovanni.espinosa00@gmail.com>
2025-01-17 00:06:59 +0800

    add password-ban

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,6 +8,7 @@ function LogIn() {
         <div className='w-full h-screen flex items-center justify-center bg-gray-300'>
           <h1>LogIn</h1>
         <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
         </div>
         
     </>
