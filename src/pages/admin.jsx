8f996271fefc47778e1e28f79517946e9512869a
Dazzle <catc.daz07@gmail.com>
2025-01-17 21:50:01 +0800

    new

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -5,13 +5,13 @@ function Admin() {
     <>
       <div className='w-full h-screen flex items-center justify-center'> 
         <div className='w-5/6 h-5/6 bg-gray-300 rounded-3xl -mt-14 '>
-          <div className='w-full h-1/6 bg-gray-500 flex flex-col'>
-            <div className='w-full h-4/5 bg-red-400'>
-              <h1 className=' h-full flex items-center bg-blue-600 pl-8 text-4xl font-bold'>Admin</h1>
+          <div className='w-full h-1/6 flex flex-col'>
+            <div className='w-full h-4/5'>
+              <h1 className=' h-full flex items-center pl-8 text-4xl font-bold'>Admin</h1>
             </div>
           </div>
-          <div className='w-full h-5/6 bg-red-400 flex items-center '> 
-          
+          <div className='w-full h-5/6 flex items-center '> 
+
           </div>
         </div>
       </div>
